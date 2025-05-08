69d89710d0d9756495eec34424a481ca17a27204
IlyaZabaluev <ilya.zabaluev.89@mail.ru>
2025-05-08 14:57:35 +0300

    feat(src): adds code task-1

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,37 @@
-import "./App.css";
+import { useFetch } from "./useFetch";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 export const App = () => {
-  return <></>;
+  const { data, isLoading, error, refetch } = useFetch<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() =>
+            refetch({
+              params: {
+                _limit: 3,
+              },
+            })
+          }
+        >
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && "Загрузка..."}
+      {error && "Произошла ошибка"}
+      {data &&
+        !isLoading &&
+        data.map((item) => <div key={item.id}>{item.title}</div>)}
+    </div>
+  );
 };
